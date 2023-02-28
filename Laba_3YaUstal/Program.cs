613d250fb73b3671c6688ba58de09571f4e0e470
Goylord12 <Renden15@yandex.ru>
2023-02-28 14:19:54 +0300

    Laba_3

---
+++ Laba_3YaUstal/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+enum DayOfWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+enum TimeOfDay
+{
+    Morning,
+    Day,
+    Evening,
+    Night
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите день недели:");
+        string inputDay = Console.ReadLine();
+
+        Console.WriteLine("Введите время дня:");
+        int inputTime = int.Parse(Console.ReadLine());
+
+        DayOfWeek dayOfWeek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), inputDay);
+        TimeOfDay timeOfDay = TimeOfDay.Night;
+
+        if (inputTime >= 7 && inputTime <= 12)
+        {
+            timeOfDay = TimeOfDay.Morning;
+        }
+        else if (inputTime >= 13 && inputTime <= 18)
+        {
+            timeOfDay = TimeOfDay.Day;
+        }
+        else if (inputTime >= 18 && inputTime <= 23)
+        {
+            timeOfDay = TimeOfDay.Evening;
+        }
+
+        if (inputTime >= 0 && inputTime <= 6)
+        {
+            Console.WriteLine("Сейчас {0}, {1}", dayOfWeek, TimeOfDay.Night);
+        }
+        else
+        {
+            Console.WriteLine("Сейчас {0}, {1}", dayOfWeek, timeOfDay);
+        }
+    }
+}
